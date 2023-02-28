e515be4cd5697e4fe12b369b4444b82e9d22879d
prayaagvenkat <125396895+prayaagvenkat@users.noreply.github.com>
2023-02-28 14:36:23 -0500

    Minor updates

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # distributed-p2
+
+
+# TODO
+- The (virtual) machine should listen on one or more sockets for such messages. Each of your virtual machines should connect to each of the other virtual machines so that messages can be passed between them
+- How to get global time?
+
+- Then, run the scale model at least 5 times
+- drift in the values of the local logical clocks in the different machines (you can get a god’s eye view because of the system time)
+- Collect in list and plot:
+    - size of the jumps in the values for the logical clocks
+    - drift in the values of the local logical clocks in the different machines
+    - the impact different timings on such things as gaps in the logical clock values and length of the message queue.
+-  try running it with a smaller variation in the clock cycles
--- sim.py
+++ sim.py
@@ -6,6 +6,8 @@ PROCESSER_SPEED = [1, 5, 10]
 NUM_PROCESSERS = len(PROCESSER_SPEED)
 INTERNAL_CLOCK = [0] * NUM_PROCESSERS
 MESSAGE_QUEUE = [[]] * NUM_PROCESSERS
+DURATION = 10
+
 
 def get_action():
     
@@ -16,24 +18,34 @@ def get_action():
 
 
 def execute(processer_id):
-    INTERNAL_CLOCK[processer_id] += 1
+    
 
     if len(MESSAGE_QUEUE[processer_id]) > 0:
         _, _, new_clock = MESSAGE_QUEUE[processer_id].pop(0)
+        INTERNAL_CLOCK[processer_id] += 1
+
         INTERNAL_CLOCK[processer_id] = max(INTERNAL_CLOCK[processer_id], new_clock)
-        write_to_log(processer_id, "Received message at time {}".format(INTERNAL_CLOCK[processer_id]))
-
-    action = get_action()
-
-    if action in [1, 2, 3]:
-        if action == 1 or action == 3:
-            MESSAGE_QUEUE[(processer_id+1)%3].append(("send", processer_id, INTERNAL_CLOCK[processer_id]))
-        if action == 2 or action == 3:
-            MESSAGE_QUEUE[(processer_id+2)%3].append(("send", processer_id, INTERNAL_CLOCK[processer_id]))
-
-        write_to_log(processer_id, "Sent message {} at time {}".format(action, INTERNAL_CLOCK[processer_id]))
+        write_to_log(processer_id, "Received message at local time {}, queue length {}".format(INTERNAL_CLOCK[processer_id], len(MESSAGE_QUEUE[processer_id])))
     else:
-        write_to_log(processer_id, "No external action taken at time {}".format(INTERNAL_CLOCK[processer_id]))
+        action = get_action()
+
+        
+        if action == 1:
+            MESSAGE_QUEUE[(processer_id+1)%3].append(("send", processer_id, INTERNAL_CLOCK[processer_id]))
+            INTERNAL_CLOCK[processer_id] += 1
+            write_to_log(processer_id, "Sent message {} at local time {}".format(action, INTERNAL_CLOCK[processer_id]))
+        elif action == 2:
+            MESSAGE_QUEUE[(processer_id+2)%3].append(("send", processer_id, INTERNAL_CLOCK[processer_id]))
+            INTERNAL_CLOCK[processer_id] += 1
+            write_to_log(processer_id, "Sent message {} at local time {}".format(action, INTERNAL_CLOCK[processer_id]))
+        elif action == 3:    
+            MESSAGE_QUEUE[(processer_id+1)%3].append(("send", processer_id, INTERNAL_CLOCK[processer_id]))
+            MESSAGE_QUEUE[(processer_id+2)%3].append(("send", processer_id, INTERNAL_CLOCK[processer_id]))
+            INTERNAL_CLOCK[processer_id] += 1
+            write_to_log(processer_id, "Sent double message {} at local time {}".format(action, INTERNAL_CLOCK[processer_id]))
+        else:
+            INTERNAL_CLOCK[processer_id] += 1
+            write_to_log(processer_id, "Internal action at local time {}".format(INTERNAL_CLOCK[processer_id]))
 
 def write_to_log(processer_id, message):
     with open("logs/{}.txt".format(processer_id), "a") as f:
@@ -51,11 +63,12 @@ def run():
         
 
     real_timestamp = 0
-
-    while True:
+    print("Running for {} seconds".format(DURATION))
+    end = time.time() + DURATION
+    while time.time() < end:
         real_timestamp += 1
         for i, processer_speed in enumerate(PROCESSER_SPEED):
-            if real_timestamp % processer_speed == 0:
+            for _ in range(processer_speed):
                 execute(i)
 
 
